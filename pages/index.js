0e3ae4885b021b7587c33e0307ecbb90317a4176
nata18sha <nata18sha@gmail.com>
2022-04-15 21:08:07 +0300

    CategoryLabel component added

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import Link from 'next/link';
-import Layout from '../components/Layout';
+import Layout from '@/components/Layout';
 import Post from '@/components/Post';
-import { sortByDate } from '../utils';
+import { sortByDate } from '@/utils/index';
 
 const HomePage = ({ posts }) => {
     return (
